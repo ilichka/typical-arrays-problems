515cc9e870703f0e4985ab798e1ed9d9d1d8d72e
ilichka <60748637+ilichka@users.noreply.github.com>
2020-03-21 20:44:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
+exports.min = function min(array) {
+    if (!array || array.length===0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if(!array || array.length===0) {
+        return 0;
+    } else {
+        let max = array[0];
+        if (array === []) {
+            return 0;
+        } else {
+            for (let i = 1; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let k = 0, avg = 0;
+    if (!array || array.length===0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            k++;
+            avg += array[i];
+        }
+        return avg / k;
+    }
+};
